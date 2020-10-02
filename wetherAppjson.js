c3b2876fce5757977cea27e28994908a0471ed2a
Munsif <munsifalimisri69@gmail.com>
2020-10-02 05:13:13 -0700

    json work done

--- wetherAppjson.js
+++ wetherAppjson.js
@@ -1,36 +1,33 @@
 const http = require("http");
 const fs = require("fs");
 const requests = require("requests");
+const express = require("express");
 const readFile = fs.readFileSync("wetherApp.html", "utf-8");
-const setValues = (readFile, apiDataVal) => {
-    console.log(apiDataVal.name);
-    // let returndata = readFile.replace("{%degreeVal%}", apiDataVal.main.temp);
-    // returndata = readFile.replace("{%country%}", apiDataVal.sys.country);
-    let returndata = readFile.replace("{%location%}", apiDataVal.name);
-    //console.log(apiDataVal.main);
+
+const app = express();
+app.use('/assets', express.static('assets'));
+const setValues = (readFile, val) => {
+    let returndata = readFile.replace("{%degree%}", val.main.temp);
+    returndata = returndata.replace("{%country%}", val.sys.country);
+    returndata = returndata.replace("{%location%}", val.name);
+    returndata = returndata.replace("{%tempIconStatus%}", val.weather[0].main);
     return returndata;
 }
-// console.log(readFile);
 const server = http.createServer((req, res) => {
     if (req.url == "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
         //must have to write http:// else u will gut Unsupported protocol "undefined" error 
-        requests("http://api.openweathermap.org/data/2.5/weather?q=Islamabad&appid=4331b45d1fe637fef6296514745974f8")
+        requests(
+            "http://api.openweathermap.org/data/2.5/weather?q=Islamabad&appid=7428d86cddd52bb1813e331885cc6b27"
+        )
             .on("data", (chunk) => {
-                const convertingChunkIntoJsData = JSON.parse(chunk);
+                const objdata = JSON.parse(chunk);
                 //converting object of js into array means (array of an object)
-                const wetArray = [convertingChunkIntoJsData];
-                res.write("this is home page");
-                const realApiData = wetArray.map(val => {
-                    setValues(readFile, val);
-                });
-
-
-
-                console.log(typeof (realApiData));
-                console.log(setValues());
-                console.log(realApiData);
+                const wetArray = [objdata];
+                const realApiData = wetArray
+                    .map((val) => setValues(readFile, val))
+                    .join("");
                 res.write(realApiData);
-                console.log(wetArray[0].main.temp);
             })
             .on("end", (err) => {
                 if (err) {
@@ -38,6 +35,9 @@ const server = http.createServer((req, res) => {
                 }
                 res.end();
             });
+    }
+    else {
+        res.end("File not found");
     }
 });
 server.listen(8000, '127.0.0.1', () => {
